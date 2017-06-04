b2d521afb6633cea4275f590bed02b631a1c8bf6
Unknown <tridjito@yahoo.com>
2017-06-04 10:35:39 -0500

    add boot 03

---
+++ boot03-170603/src/main/java/com/djitz/boots/boot03170603/Boot03170603Application.java
@@ -0,0 +1,12 @@
+package com.djitz.boots.boot03170603;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot03170603Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot03170603Application.class, args);
+	}
+}
---
+++ boot03-170603/src/test/java/com/djitz/boots/boot03170603/Boot03170603ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.djitz.boots.boot03170603;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Boot03170603ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
